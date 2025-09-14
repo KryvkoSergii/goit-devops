9a6aa7f3d03034dfc47ccd52bf4aa79d92d10bd6
Sergii Kryvko <kryvkosergii@gmail.com>
2025-09-14 22:10:28 +0300

    * seed job jenkins

--- modules/jenkins/variables.tf
+++ modules/jenkins/variables.tf
@@ -2,3 +2,28 @@ variable "cluster_name" {
   description = "Name of Kubernetes cluster"
   type        = string
 }
+
+variable "oidc_provider_arn" {
+  description = "ARN of the OIDC provider"
+  type        = string
+}
+
+variable "oidc_provider_url" {
+  description = "URL of the OIDC provider"
+  type        = string
+}
+
+variable "github_pat" {
+  description = "GitHub Personal Access Token"
+  type        = string
+}
+
+variable "github_user" {
+  description = "GitHub username"
+  type        = string
+}
+
+variable "github_repo_url" {
+  description = "GitHub repository name"
+  type        = string
+}
--- outputs.tf
+++ outputs.tf
@@ -13,15 +13,27 @@ output "ECR_repository_url" {
   value       = module.ecr.repository_url
 }
 
-output "postgres_endpoint" {
-  description = "PostgreSQL RDS instance endpoint"
-  value       = module.rds.db_instance_endpoint
+output "oidc_provider_arn" {
+  description = "OIDC Provider ARN"
+  value       = module.eks.oidc_provider_arn
 }
 
+output "oidc_provider_url" {
+  description = "OIDC Provider URL"
+  value       = module.eks.oidc_provider_url
+}
+
+#-------------Jenkins-----------------
 output "jenkins_release" {
   value = module.jenkins.jenkins_release_name
 }
 
 output "jenkins_namespace" {
   value = module.jenkins.jenkins_namespace
+}
+
+#-------------RDS-----------------
+output "postgres_endpoint" {
+  description = "PostgreSQL RDS instance endpoint"
+  value       = module.rds.db_instance_endpoint
 }
