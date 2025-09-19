8fff3f7f61f8ed70dd36017e6b8b00c1f9bcd18a
Sergii Kryvko <kryvkosergii@gmail.com>
2025-09-19 23:58:47 +0300

    * fix skip_final_snapshot on RDS

--- modules/rds/rds.tf
+++ modules/rds/rds.tf
@@ -15,6 +15,8 @@ resource "aws_db_instance" "standard" {
   backup_retention_period = var.backup_retention_period
   parameter_group_name    = aws_db_parameter_group.standard[0].name
 
+  skip_final_snapshot = true
+
   tags = var.tags
 }
 
